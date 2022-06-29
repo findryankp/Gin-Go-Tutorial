e027bbb0412bba23b83e769d70bb902cf73b416e
Findryankp <findryankurnia@gmail.com>
2022-06-29 14:14:45 +0700

    Get Hello World

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.New()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello World!",
+		})
+	})
+	r.Run()
+}
