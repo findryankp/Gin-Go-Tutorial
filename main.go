e53a46ada8b6461095162b5f727fca17fb1473c1
Findryankp <findryankurnia@gmail.com>
2022-06-29 14:18:39 +0700

    add data

--- main.go
+++ main.go
@@ -6,12 +6,29 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Book struct {
+	ID     string `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+var books = []Book{
+	{ID: "1", Title: "Harry Potter", Author: "J. K. Rowling"},
+	{ID: "2", Title: "The Lord of the Rings", Author: "J. R. R. Tolkien"},
+	{ID: "3", Title: "The Wizard of Oz", Author: "L. Frank Baum"},
+}
+
 func main() {
 	r := gin.New()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello World!",
+			"message": "API GIN GO",
 		})
 	})
+
+	r.GET("/books", func(c *gin.Context) {
+		c.JSON(http.StatusOK, books)
+	})
+
 	r.Run()
 }
